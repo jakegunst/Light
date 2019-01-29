d5dd400c9e741d94368f55c865eedae96439d67b
Jake Gunst <jacobwgunst@gmail.com>
2019-01-29 17:35:38 -0600

    My first commit. This should be a working version of the flashlight app I created for project #1.

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
-}
-
+ var lightOn = true
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    }
